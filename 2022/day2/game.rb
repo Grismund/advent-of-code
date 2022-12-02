e7a0292fe6edbb6c7fb0e3840464edc6daea5f24
Seth Faerber <seth.faerber@ramseysolutions.com>
2022-12-02 15:26:01 -0600

    Success

--- 2022/day2/game.rb
+++ 2022/day2/game.rb
@@ -1,64 +1,82 @@
 class Game
 
-  attr_accessor :result, :my_score, :elf, :me
+  attr_accessor :result, :my_score, :elf, :me, :elf_score, :my_choice, :elf_choice
 
-  def initialize (result = "", my_score = 0, elf = "", me = "")
+  def initialize (result = "", my_score = 0, elf = "", me = "", elf_score = 0, my_choice = 0, elf_choice = 0)
     @result = result
     @my_score = my_score
+    @elf_score = elf_score
     @elf = elf
     @me = me
+    @my_choice = my_choice
+    @elf_choice = elf_choice
   end
 
-    elf_rock = "A"
-    elf_paper = "B"
-    elf_scissors = "C"
-    me_rock = "X"
-    me_paper = "Y"
-    me_scissors = "Z"
+  def letter_to_rps
+    @elf = "rock" if @elf == "A"
+    @elf = "paper" if @elf =="B"
+    @elf = "scissors" if @elf == "C"
+
+    @me = "rock" if @me == "X"
+    @me = "paper" if @me == "Y"
+    @me = "scissors" if @me == "Z"
+    puts "Elf's #{ @elf } vs My #{ @me }"
+  end
+
+  def choice_points
+    @elf_choice = 1 if @elf == "rock"
+    @elf_choice = 2 if @elf =="paper"
+    @elf_choice = 3 if @elf == "scissors"
+
+    @my_choice = 1 if @me == "rock"
+    @my_choice = 2 if @me == "paper"
+    @my_choice = 3 if @me == "scissors"
+  end
+
+  def compare
+    if @elf == @me
+      @result = "tie"
+    elsif
+    (@elf == "rock" && @me == "paper") ||
+    (@elf == "paper" && @me == "scissors") ||
+    (@elf == "scissors" && @me == "rock")
+      @result = "win"
+    else
+      @result = "loss"
+    end
+  end
+
+  def award_points
+    case result
+    when "win"
+      @my_score += @my_choice + 6
+      @elf_score += @elf_choice
+      puts "I win with #{@my_choice + 6} points!\n\n"
+    when "tie"
+      @my_score += @my_choice + 3
+      @elf_score += @elf_choice + 3
+      puts "We tied with #{@my_choice + 3 } points!\n\n"
+    when "loss"
+      @my_score += @my_choice
+      @elf_score += @elf_choice + 6
+      puts "Elf won with #{@elf_choice + 6} points! BOO!!!\n\n"
+    end
+  end
+
   def execute_game
     File.readlines("turns.csv").each do |line|
-      puts line
+      @elf = line[0]
+      @me = line[2]
 
+      letter_to_rps
+      choice_points
+      compare
+      award_points
 
-      (@elf = "rock") if (line[0] == "A")
-      (@elf = "paper") if (line[0] =="B")
-      (@elf = "scissors") if (line[0] == "C")
-
-      (@me = "rock") if (line[0] == "X")
-      (@me = "paper") if (line[0] == "Y")
-      (@me = "scissors") if (line[0] == "Z")
-
-      # @elf == @me ? puts "tie" : "someone won."
-      puts "#{@elf} vs #{@me}"
-      # round
-    end
-  end
-
-  def round
-    if @elf == me
-      result = "tie"
-    elsif (elf == rock && me == paper) ||
-      (elf == paper && me == scissors) ||
-      (elf == scissors && me == rock)
-      result = "win"
-    else
-      result = "loss"
-    end
-  end
-
-
-
-  def score
-    case result
-
-    when "win"
-      my_score += points
-    when "tie"
-      my_score += points
-    when "loss"
-      my_score += points
     end
 
-
+    puts "\nFinal score is:\n
+Elf: #{@elf_score}
+Me: #{@my_score}"
   end
 end
