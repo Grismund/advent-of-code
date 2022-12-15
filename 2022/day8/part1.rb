5499343fef6f706ec03e3e023eb9a77475d78609
Seth Faerber <seth.faerber@ramseysolutions.com>
2022-12-15 11:17:03 -0600

    loops working. Needs conditional

--- 2022/day8/part1.rb
+++ 2022/day8/part1.rb
@@ -2,23 +2,28 @@
 @tree_rows = []
 @tree_columns = []
 
+
 File.readlines("data.csv").each do |line|
-    tree_rows << line.chomp.split("")
+  @tree_rows << line.chomp.split("")
 end
 
 def sort_tree_columns
-    index = 0
+    column_index = 0
+    tree_index = 0
 
     @tree_rows.each do |row|
-        if row
-            @tree_columns[index] << row[index]
-        else
-            index += 1
+        row.each do |tree|
+
+            # TODO: Needs a conditionl... if nested array doesn't exist, create one then shove tree
+            # TODO: otherwise, if it does exist, just shove the tree.
+            @tree_columns[column_index] = Array.new(tree_index) { tree }
+            column_index += 1
+        end
+        tree_index += 1
+        column_index = 0
     end
-
-
 
 end
 
-
-pp tree_rows
+sort_tree_columns
+pp @tree_columns
