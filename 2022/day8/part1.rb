cbcccc90524ec1572cae2a4d6df460acf3d9a6cf
Seth Faerber <seth.faerber@ramseysolutions.com>
2022-12-20 16:06:12 -0600

    must initialize instance variables. Only n00bs forget ;)

--- 2022/day8/part1.rb
+++ 2022/day8/part1.rb
@@ -1,37 +1,75 @@
 # transpose
+class TreeCounter
+    def initialize
+        @formatted_data = []
+        @tree_rows = []
+        @tree_columns = [[]]
+        @tree_count = 0
 
+    end
 
-@tree_rows = []
-@tree_columns = [[]]
+    def run
+        # format_data
+        # tree_row_assignment
+        chomp_data
 
+        # tree_row_assignment
+        # puts "\n -------------------------------------------- \n#{@tree_rows}"
+        # @tree_rows.each do |tree|
+        #     on_edge(tree, index)
+        #     # puts "#{tree index}"
+        # end
+        # pp @tree_rows
+    end
 
-File.readlines("data.csv").each do |line|
-  @tree_rows << line.chomp.split("")
-end
-
-def sort_tree_columns
-    column_index = 0
-    tree_index = 0
-
-    @tree_rows.each do |row|
-        row.each do |tree|
-
-            @tree_columns[column_index] ||= Array.new(tree_index) { tree }
-
-
-            if @tree_columns[column_index][tree_index]
-                @tree_columns[column_index][tree_index] = tree
-                column_index += 1
-            else
-                @tree_columns[column_index] = Array.new(tree_index) { tree }
-                column_index += 1
-            end
+    def format_data
+        chomped_data = @formatted_data
+        # pp chomped_data
+        chomped_data.each do |line|
+            line.split("\n")
         end
-        tree_index += 1
-        column_index = 0
+        # pp @formatted_data
+    end
+
+    def chomp_data
+        File.readlines("data.csv").each do |line|
+            chomped_data = line.chomp
+            @formatted_data << chomped_data
+        end
+        pp @formatted_data
+    end
+    def tree_row_assignment
+        # @tree_rows.transpose
+        # pp @formatted_data
+    end
+
+    def tree_column_assignment
+        @tree_columns = @tree_rows.transpose
+    end
+
+    def on_edge(tree, index)
+        @tree_count += 1 if index == 0
+    end
+
+    def tallest_from_edge
+
+    end
+
+    def uncount_single_tallest
+        if tree_row.tallest_tree.uniq
+            tree_count - 1
+        end
+    end
+
+
+    def count_visible
+        on_edge
+        tallest_from_edge
     end
 
 end
 
-sort_tree_columns
-pp @tree_columns
+
+run = TreeCounter.new
+run.run
+
